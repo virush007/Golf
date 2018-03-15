34c7cbd3e021c4938c92062054cfdcbfefabb0bd
virush007 <huzaifayaseen007@gmail.com>
2018-03-15 19:48:43 +0500

    End Of Day

--- Golf Mini/Assets/Scripts/BallController.cs
+++ Golf Mini/Assets/Scripts/BallController.cs
@@ -1,24 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BallController : MonoBehaviour {
 
-    public float speed =100f;
+    public float speed = 1f;
+    public Image powerBar;
+    bool high = false;
 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    void changePower()
+    {
+
+        if (high)
+        {
+            speed++;
+        }
+        else
+        {
+            speed--;
+        }
+        if (speed == 100)
+        {
+            high = false;
+        }
+        if (speed == 0)
+        {
+            high = true;
+        }
+        powerBar.fillAmount = speed/100;
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+        powerBar.fillAmount = 0;
+        InvokeRepeating("changePower", 0.01f, 0.01f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
         if (Input.GetMouseButtonDown(0))
         {
             //GetComponent<Rigidbody>().velocity = transform.forward * speed * Time.deltaTime;
-            GetComponent<Rigidbody>().AddForce(transform.forward * speed * Time.deltaTime);
+            GetComponent<Rigidbody>().velocity = transform.forward * speed * Time.deltaTime *10;
         }
-	}
+    }
+
+
+    
+
+
+
 }
