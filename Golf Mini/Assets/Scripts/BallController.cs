99408fb8ebddf4beb689fb169b28ac959d4366c3
virush007 <huzaifayaseen007@gmail.com>
2018-03-15 16:53:47 +0500

    Starting Project

---
+++ Golf Mini/Assets/Scripts/BallController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    public float speed =100f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            //GetComponent<Rigidbody>().velocity = transform.forward * speed * Time.deltaTime;
+            GetComponent<Rigidbody>().AddForce(transform.forward * speed * Time.deltaTime);
+        }
+	}
+}
